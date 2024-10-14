d4cc5512bea816a0c294d9e8a0507eb57546fb20
wgirten-packt <160566569+wgirten-packt@users.noreply.github.com>
2024-10-14 10:21:34 -0400

    Update Catalog Name
    Update the catalog name in example notebook to match diagrams.

--- chapter05/Data Lineage Demo.sql
+++ chapter05/Data Lineage Demo.sql
@@ -1,14 +1,14 @@
 -- Databricks notebook source
-CREATE CATALOG IF NOT EXISTS bmdadl_chp5;
+CREATE CATALOG IF NOT EXISTS building_modern_dapps;
 
 -- COMMAND ----------
 
-CREATE SCHEMA IF NOT EXISTS bmdadl_chp5.lineage_demo;
+CREATE SCHEMA IF NOT EXISTS building_modern_dapps.lineage_demo;
 
 -- COMMAND ----------
 
 CREATE TABLE IF NOT EXISTS
-  bmdadl_chp5.lineage_demo.taxi_drivers (
+  building_modern_dapps.lineage_demo.taxi_drivers (
     driver_id INT,
     max_passengers INT,
     car_make string,
@@ -17,20 +17,20 @@ CREATE TABLE IF NOT EXISTS
     car_year INT
   );
 
-INSERT INTO bmdadl_chp5.lineage_demo.taxi_drivers
+INSERT INTO building_modern_dapps.lineage_demo.taxi_drivers
     (driver_id, max_passengers, car_make, car_model, car_color, car_year)
 VALUES
     (101, 4, "Ford", "Crown Victoria", "Yellow", 2004),
     (102, 3, "Lincoln", "MKT", "Black", 2019),
     (3396, 7, "Chevrolet","Suburban", "Grey", 2021);
 
-CREATE TABLE IF NOT EXISTS bmdadl_chp5.lineage_demo.taxi_drivers_refined
+CREATE TABLE IF NOT EXISTS building_modern_dapps.lineage_demo.taxi_drivers_refined
 AS SELECT
   driver_id, max_passengers, concat(car_color, " ", car_year, " ", car_make," ", car_model)
 AS
   car_description
 FROM
-  bmdadl_chp5.lineage_demo.taxi_drivers;
+  building_modern_dapps.lineage_demo.taxi_drivers;
 
 
 -- COMMAND ----------
